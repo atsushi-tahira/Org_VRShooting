6f18856d6281bac85c24ea6e65df01af35ae0577
atsushitahira <a.tahira3@icloud.com>
2019-08-16 17:58:06 +0900

    VRShooting 作成
    VR上で銃の練習をするゲーム
    現在の構想としては、的を撃ち抜いて点数を競うゲームにする予定
    将来的には、ワンルームの中で迫り来るゾンビを撃ち抜くゲームにする

---
+++ Assets/RaySC.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySC : MonoBehaviour {
+
+    Ray ray;
+
+	void Update () {
+        ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position,transform.forward * 10f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit,10f))
+        {
+            string score = hit.collider.gameObject.tag;
+            Debug.Log("Score is " + score);
+        }
+    }
+}
