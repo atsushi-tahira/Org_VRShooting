2eee34c71285f36ccb3154bedb998af5b3c02e3a
atsushitahira <a.tahira3@icloud.com>
2019-08-21 17:32:03 +0900

    VRShooting 機能追加
    ・リロード機能と残弾数を追加
    スペースキー押下で弾発射、Rキー押下でリロード
    弾の可視化は未実装、音声のみ追加

--- Assets/RaySC.cs
+++ Assets/RaySC.cs
@@ -4,25 +4,97 @@ using UnityEngine;
 
 public class RaySC : MonoBehaviour {
 
-    Ray ray;
+    public AudioClip handGunSE;
+    public AudioClip jamSE;
+    public AudioClip reloadSE;
+    public TextMesh bulletLast;
+    public GameObject noBulletText;
 
-	void Update () {
+    Ray ray;
+    AudioSource audioS;
+    int ammo = 6;
+    int havedBullet = 18;
+    int handGunAmmo = 6;
+
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        ammo = 6;
+    }
+
+    void Update () {
         ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(transform.position,transform.forward * 10f);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //ショット
+        if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
-            Shoot();
+            if (ammo != 0)
+            {
+                Shoot();
+            }
+            else 
+            {
+                AudioPlay(jamSE);
+                StartCoroutine(NoBullet("弾切れ"));
+            }
         }
+
+        //リロード
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AudioPlay(reloadSE);
+            //ammo = 6;
+            //havedBullet -= 6;
+            for (int i = 0;i < handGunAmmo;i++)
+            {
+                if (ammo != handGunAmmo && havedBullet != 0)
+                {
+                    ammo++;
+                    havedBullet--;
+                }
+                else if (havedBullet == 0)
+                {
+                    StartCoroutine(NoBullet("リロード不可"));
+                }
+                else if (ammo == handGunAmmo)
+                {
+                    break;
+                }
+                
+            }
+        }
+
+        bulletLast.text = "残弾：" + ammo + "/" + havedBullet ;
+
 	}
 
     void Shoot()
     {
+
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit,10f))
         {
+            AudioPlay(handGunSE);
+
             string score = hit.collider.gameObject.tag;
             Debug.Log("Score is " + score);
+            ammo--;
         }
+    }
+
+    void AudioPlay(AudioClip source)
+    {
+        audioS.clip = source;
+        audioS.Play();
+    }
+
+    IEnumerator NoBullet(string str)
+    {
+        noBulletText.GetComponent<TextMesh>().text = str;
+        noBulletText.SetActive(true);
+        yield return new WaitForSeconds(0.8f);
+        noBulletText.SetActive(false);
     }
 }
